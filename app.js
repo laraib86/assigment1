88037355eba6bbf896923af8e06f6314f742d499
laraib <u2018295@giki.edu.pk>
2022-08-22 16:15:54 +0500

    msg updated

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+
+
+  res.end('Hello brother kam ho gia' );
 });
 
 server.listen(port, hostname, () => {
